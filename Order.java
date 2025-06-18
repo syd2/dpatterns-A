670270dfe364c05a89b3c773ff39ba607534271e
syd2 <syd.jr7@gmail.com>
2025-06-18 15:22:12 -0400

    full feature implemented

--- Order.java
+++ Order.java
@@ -1,28 +1,46 @@
 
 import java.util.List;
 
+import java.util.ArrayList;
+
 public class Order {
-    private String id;
-    private int    itemCount;
-    private double itemCost;
-    private double shippingCost;
-    private double discount;
-    private List<OrderObserver> observers;
+    private String                id;
+    private int                   itemCount    = 0;
+    private double                itemCost     = 0.0;
+    private double                shippingCost = 10.0;  // default
+    private double                discount     = 0.0;
+    private List<OrderObserver>   observers    = new ArrayList<>();
 
     public Order(String id) {
-
+        this.id = id;
     }
 
-    public void registerObserver(OrderObserver o) { }
-    public void removeObserver(OrderObserver o) { }
-    public void notifyObservers() { }
+    public void registerObserver(OrderObserver o) {
+        observers.add(o);
+    }
+    public void removeObserver(OrderObserver o) {
+        observers.remove(o);
+    }
+    public void notifyObservers() {
+        for (OrderObserver o : observers) {
+            o.update(this);
+        }
+    }
 
-    public void addItem(double price) { }
+    public void addItem(double price) {
+        itemCount++;
+        itemCost += price;
+        notifyObservers();
+    }
 
-    // getters only
-    public int    getItemCount()     { return 0; }
-    public double getItemCost()      { return 0; }
-    public double getShippingCost()  { return 0; }
-    public double getDiscount()      { return 0; }
-    public double getTotalCost()     { return 0; }
+    public int    getItemCount()    { return itemCount; }
+    public double getItemCost()     { return itemCost; }
+    public double getShippingCost() { return shippingCost; }
+    public double getDiscount()     { return discount; }
+    public double getTotalCost()    { return itemCost + shippingCost - discount; }
+
+    // internal hooks for observers
+    void applyDiscount(double amount)   { discount = amount; }
+    void setShippingCost(double cost)   { shippingCost = cost; }
 }
+
--- PriceObserver.java
+++ PriceObserver.java
@@ -1,5 +1,9 @@
-
 public class PriceObserver implements OrderObserver {
     @Override
-    public void update(Order order) { /* signature only */ }
+    public void update(Order order) {
+        if (order.getTotalCost() > 200.0) {
+            order.applyDiscount(20.0);
+            System.out.println("PriceObserver applied $20 discount. New total: " + order.getTotalCost());
+        }
+    }
 }
